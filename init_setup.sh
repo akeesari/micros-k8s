ed958c154c2e0d0ae22343531abd2f9689bf97df
Anji Keesari <anjkeesari@gmail.com>
2023-01-02 08:33:46 -0800

    initial setup

---
+++ init_setup.sh
@@ -0,0 +1,12 @@
+echo [$(date)]: "START"
+export _VERSION_=3.8
+echo [$(date)]: "creating environment with python ${_VERSION_}"
+conda create --prefix ./env python=${_VERSION_} -y
+echo [$(date)]: "activate environment"
+source activate ./env
+echo [$(date)]: "install requirements"
+pip install -r requirements.txt
+echo [$(date)]: "END"
+
+# to remove everything -
+# rm -rf env/ .gitignore conda.yaml README.md .git/
